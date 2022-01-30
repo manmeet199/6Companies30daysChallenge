cb01ea08ff0e989daae56f69ba8f04976b97471d
Manmeet Singh Chhabra <manmeett29@gmail.com>
2022-01-30 12:30:42 +0530

    Walmart

---
+++ Walmart/08_Maximum_Height_tree.cpp
@@ -0,0 +1,37 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int height(int N){
+        // code here
+     int i=0;
+for(i=0;i<1000;i++)
+{
+if(((i*(i+1))/2)>N)
+break;
+}
+return i-1;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        cout<<ob.height(N)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
